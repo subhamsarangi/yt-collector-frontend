62ed1c331f6c3603de05c2763b6aa8877c822490
subhamsarangi <subhamsarangi2016@gmail.com>
2026-04-10 22:01:49 +0530

    login page logo fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,7 @@ import LoginForm from "@/components/LoginForm";
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[70vh] gap-6">
-      <div className="flex items-center gap-3">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/logo.png" alt="YT Collector" width={40} height={40} className="rounded-lg" />
-        <h1 className="text-2xl font-bold">YT Collector</h1>
-      </div>
+      <h1 className="text-2xl font-bold">YT Collector</h1>
       <LoginForm />
     </div>
   );
